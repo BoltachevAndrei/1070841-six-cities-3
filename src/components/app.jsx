c85100d99f680c1b92724071820df6cf6d1f0994
BoltachevAndrei <andy_bolt@mail.ru>
2020-02-07 01:20:09 +0400

    Личный проект: валидируй, как никогда не валидировал

--- src/components/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Main from './main.jsx';
-
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  return <Main places={props.places}/>;
-};
-
-export default App;
