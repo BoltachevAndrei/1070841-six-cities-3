68c59668c4a5f5b97d6dee135f2356d418ea2c46
BoltachevAndrei <andy_bolt@mail.ru>
2020-02-13 18:11:22 +0400

    Проводит рефакторинг, убирает повторяющиеся участки кода

--- src/components/place-card/place-card.jsx
+++ src/components/place-card/place-card.jsx
@@ -8,19 +8,8 @@ const PlaceCard = (props) => {
     (<div className="place-card__mark">
       <span>Premium</span>
     </div>) : ``;
-  const placeIsBookmarked = isBookmarked ?
-    (<button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-      <svg className="place-card__bookmark-icon" width="18" height="19">
-        <use xlinkHref="#icon-bookmark"></use>
-      </svg>
-      <span className="visually-hidden">In bookmarks</span>
-    </button>) :
-    (<button className="place-card__bookmark-button button" type="button">
-      <svg className="place-card__bookmark-icon" width="18" height="19">
-        <use xlinkHref="#icon-bookmark"></use>
-      </svg>
-      <span className="visually-hidden">To bookmarks</span>
-    </button>);
+  const placeIsBookmarkedClass = isBookmarked ? `place-card__bookmark-button place-card__bookmark-button--active button` : `place-card__bookmark-button button`;
+  const placeIsBookmarkedText = isBookmarked ? `In bookmarks` : `To bookmarks`;
 
   return (
     <article className="cities__place-card place-card" onMouseOver={() => props.onPlaceCardMouseOver(id)}>
@@ -36,7 +25,12 @@ const PlaceCard = (props) => {
             <b className="place-card__price-value">&euro;{price ? price : ``}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          {placeIsBookmarked}
+          <button className={placeIsBookmarkedClass} type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">{placeIsBookmarkedText}</span>
+          </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
